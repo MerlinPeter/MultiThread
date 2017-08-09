41fcc36fd2437861a5674b0b793dec0e8ec95a44
MerlinPeter <merlinahila@gmail.com>
2017-08-09 09:10:47 -0700

    project commit

---
+++ src/MultithreadRunnable.java
@@ -0,0 +1,13 @@
+
+public class MultithreadRunnable implements Runnable{
+
+	public void run(){
+		System.out.println("My thread is in running state.");
+	}
+	
+	public static void main(String[] args) {
+		MultithreadingDemo obj = new MultithreadingDemo();
+		Thread trdobj = new Thread(obj);
+		trdobj.start();
+	}
+}
---
+++ src/MyThread1.java
@@ -0,0 +1,12 @@
+
+public class MyThread1 extends Thread {
+    String name;
+    MyThread1(String name){
+    	this.name = name;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
